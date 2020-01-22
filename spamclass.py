7d10955b371a931790aef90d990f8d77f523e087
Ammar Chalifah <38188988+ammarchalifah@users.noreply.github.com>
2020-01-23 02:45:38 +0700

    Add files via upload

---
+++ spamclass.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+
+spamkey=pd.read_csv("spamkey.csv", usecols=['0'])
+print(spamkey)
+
+spamlist=[]
+for i in spamkey['0']:
+    spamlist.append(i)
+
+print(spamlist)
+
+#Number of tolerance
+numb=2
+
+test=open("testtext.txt","r")
+textoken=[]
+for x in test:
+    print(x)
+    temp=word_tokenize(x)
+    for b in temp:
+        textoken.append(b)
+    
+print(textoken)
+
+count=0
+for i in textoken:
+    if i in spamlist:
+        count=count+1
+    if count>=numb:
+        print("\n\nThis message is a spam")
+        exit()
+
+print("\n\nThis message is NOT a spam")
+test.close()
---
+++ spams.py
@@ -0,0 +1,88 @@
+import nltk
+import pandas as pd
+#nltk.download("punkt")
+from nltk.tokenize import word_tokenize
+
+#Read external file from spam.csv
+test_data_rough=pd.read_csv("spam.csv", encoding='latin-1')
+test_data=test_data_rough[['v1','v2']]
+print(test_data)
+
+#Load all 'spam' and 'ham' labelled texts to a list
+spam_text=test_data[test_data['v1']=='spam']
+ham_text=test_data[test_data['v1']=='ham']
+print(spam_text)
+print(ham_text)
+
+spam_text[['v1','v2']].to_csv("spamonly.csv")
+ham_text[['v1','v2']].to_csv("notspamonly.csv")
+#Create new list with most popular word from spam list and ham list with stopwords removed
+
+token_spam=[]
+token_ham=[]
+
+for i in spam_text['v2']:
+    token_spam.append(word_tokenize(i))
+
+for i in ham_text['v2']:
+    token_ham.append(word_tokenize(i))
+
+from nltk.probability import FreqDist
+
+#print(token_spam[0:1])
+#print(token_ham[0:1])
+
+tokenspam=[]
+tokenham=[]
+
+for i in token_spam:
+    for j in i:
+        tokenspam.append(j)
+for i in token_ham:
+    for j in i:
+        tokenham.append(j)
+
+spamdist=FreqDist(tokenspam)
+hamdist=FreqDist(tokenham)
+
+#print(spamdist.most_common()[0:10])
+#print(hamdist.most_common()[0:10])
+
+#Take most popular word in spam that not popular in ham
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words=set(stopwords.words("english"))
+
+impspam=[]
+for i in tokenspam:
+    if i not in stop_words:
+        impspam.append(i)
+
+impham=[]
+for i in tokenham:
+    if i not in stop_words:
+        impham.append(i)
+
+spamonly=[]
+for i in impspam:
+    if i not in impham:
+        spamonly.append(i)
+
+spamonlydist=FreqDist(spamonly)
+spamordered=spamonlydist.most_common()
+
+#masih gajelas nih ah
+spamkey=[]
+for i in spamordered:
+    if i[1]>4:
+        spamkey.append(i[0])
+
+print(spamkey)
+
+#spamkey.to_csv("spamkeys.csv")
+
+spamkeydf=pd.DataFrame(spamkey)
+spamkeydf.to_csv("spamkey.csv")
+
+
+#Create counter and labeller for text with some number of spam
